b0b48c1b88025b19a36c7f547ea2dd29db32ed30
evan-razzaque <evan.razzaque7@gmail.com>
2026-04-08 16:18:41 -0400

    Add http server

--- app/src/main/java/ca/mohawk/testo/MainActivity.java
+++ app/src/main/java/ca/mohawk/testo/MainActivity.java
@@ -1,10 +1,16 @@
 package ca.mohawk.testo;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.http.HttpEngine;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,16 +18,167 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
-    String tag = "==sessionLogging==";
+    public static MainActivity context;
+
+    private class HttpServerThread extends Thread {
+        ServerSocket httpServerSocket;
+        static final int HttpServerPORT = 8888;
+
+        @Override
+        public void run() {
+            Socket socket = null;
+
+            try {
+                httpServerSocket = new ServerSocket(HttpServerPORT);
+
+                while(true){
+                    socket = httpServerSocket.accept();
+
+                    HttpResponseThread httpResponseThread =
+                            new HttpResponseThread(socket);
+                    httpResponseThread.start();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class HttpResponseThread extends Thread {
+        Socket socket;
+        String h1;
+
+        HttpResponseThread(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            BufferedReader is;
+            PrintWriter os;
+            String request;
+
+            try {
+                is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                request = is.readLine();
+
+                while (request != null && !request.contains("Content-Length")) {
+                    request = is.readLine();
+                    Log.d(tag, "run: " + request);
+
+                }
+
+                int contentLength = -1;
+                try {
+
+                    contentLength = Integer.parseInt(request.split(":")[1].substring(1));
+                } catch (NumberFormatException e) {
+
+                }
+                if (contentLength <= 0) {
+                    return;
+                }
+
+                char buf[] = new char[contentLength + 2];
+                is.read(buf, 0, contentLength + 2);
+
+                String body = new String(buf);
+                Log.d(tag, "run body: " + body);
+
+                os = new PrintWriter(socket.getOutputStream(), true);
+
+                String response =
+                        "<html><head></head>" +
+                                "<body>" +
+                                "<h1>" + h1 + "</h1>" +
+                                "</body></html>";
+
+                os.print("HTTP/1.0 200" + "\r\n");
+                os.print("Content type: text/html" + "\r\n");
+                os.print("Content length: " + response.length() + "\r\n");
+                os.print("\r\n");
+                os.print(body + "\r\n");
+                os.flush();
+                socket.close();
+
+                MainActivity.this.runOnUiThread(() -> {
+                    MainActivity _this = MainActivity.context;
+                    TextView url = _this.findViewById(R.id.urlTextView);
+                    url.setText(body);
+                });
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return;
+        }
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        Log.d(tag, "getIpAddress: " + inetAddress.getHostAddress());
+                        ip += "SiteLocalAddress: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+    String tag = "===sessionLogging===";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        HttpServerThread httpServerThread = new HttpServerThread();
+        httpServerThread.start();
+
+        TextView urlTextView = findViewById(R.id.urlTextView);
+        urlTextView.setText(getIpAddress() + ":" + HttpServerThread.HttpServerPORT + "\n");
+
         Log.d(tag,"onCreate");
         appendTime();
     }
